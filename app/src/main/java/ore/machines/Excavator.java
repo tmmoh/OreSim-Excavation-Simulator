10007f2e0576c06f2ad82557f51b0318bea5cfff
SpencerTVaughan <stvaughan@student.unimelb.edu.au>
2024-04-18 02:14:20 +1000

    Added Internal Documentation

--- app/src/main/java/ore/machines/Excavator.java
+++ app/src/main/java/ore/machines/Excavator.java
@@ -8,6 +8,11 @@ import java.util.List;
 public class Excavator extends Machine {
     private int rocksDestroyed;
 
+    /**
+     * Constructor to initialise the Excavator with its ID.
+     *
+     * @param id    The unique identifier for the excavator.
+     * */
     public Excavator(int id) {
         super(id, true, "sprites/excavator.png", Collections.emptyList(), List.of(Rock.class));  // Rotatable
         rocksDestroyed = 0;
